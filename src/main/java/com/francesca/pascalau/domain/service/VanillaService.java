eda342ce357464060c4e6edc60060bf4d4c0a660
FrancescaPascalau <pascalaufrancescaa@gmail.com>
2022-04-05 09:19:48 +0200

    Added vanilla service to compare method implementation without AOP to log method.

--- src/main/java/com/francesca/pascalau/domain/service/VanillaService.java
+++ src/main/java/com/francesca/pascalau/domain/service/VanillaService.java
@@ -2,6 +2,7 @@ package com.francesca.pascalau.domain.service;
 
 import com.francesca.pascalau.domain.model.CustomParams;
 import com.francesca.pascalau.domain.model.CustomRequest;
+import com.francesca.pascalau.domain.model.CustomResponse;
 import com.francesca.pascalau.domain.model.Type;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,6 +14,7 @@ import java.util.UUID;
 @Service
 public class VanillaService {
 
+    //Logging the execution time of the method using the System time
     public CustomRequest logExecutionTime(CustomParams params) {
         long startTime = System.currentTimeMillis();
 
@@ -28,5 +30,26 @@ public class VanillaService {
         log.info("Method executed in: " + executionTime + " millis.");
 
         return request;
+    }
+
+    //Logging the method info using the java tools
+    public CustomResponse logInfo(CustomRequest request) {
+        String methodName = this.getClass().getEnclosingMethod().getName();
+        log.info("Current method name: " + methodName);
+
+        Class<?>[] parameterTypes = this.getClass().getEnclosingMethod().getParameterTypes();
+        log.info("Method parameter types: ");
+        for (Object parameterType : parameterTypes) {
+            log.info("\n" + parameterType);
+        }
+
+        String className = this.getClass().getSimpleName();
+        log.info("Current class name: " + className);
+
+        return CustomResponse.builder()
+                .id(UUID.randomUUID())
+                .message(request.getMessage())
+                .issuedAt(request.getCreatedAt())
+                .build();
     }
 }
